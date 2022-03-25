3ff768e80ce9d822b1d4321279886b23930acfd1
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-03-25 16:31:04 +0600

    Add SteamChat package and implement signup api
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,31 @@
 import express from 'express';
 import cors from 'cors';
+import { StreamChat } from 'stream-chat';
+import { v4 as uuidv4 } from 'uuid';
+import bcrypt from 'bcrypt';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const api_key = 'cphv78cdm95v';
+const api_secret =
+  'ne9qhss5gtvzhds5q6exfxqaar3vc8cxps3fz2u4v84j79pcba7dfmdc9965xv2w';
+
+const serverClient = StreamChat.getInstance(api_key, api_secret);
+
+app.post('/singup', async (req, res) => {
+  try {
+    const { firstName, lastName, username, password } = req.body;
+    const userId = uuidv4();
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const token = serverClient.createToken(userId);
+    res.json({ token, userId, firstName, lastName, username, hashedPassword });
+  } catch (error) {
+    res.json(error);
+  }
+});
 
 app.listen(3001, () => {
   console.log('server is running on port 3001');
